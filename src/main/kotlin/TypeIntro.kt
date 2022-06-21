5e494abe4ac32deccb899ba692f3cf46fdddeb4d
AV.Gogolev <av.gogolev@tensor.ru>
2022-06-22 00:28:30 +0500

    4

--- src/main/kotlin/TypeIntro.kt
+++ src/main/kotlin/TypeIntro.kt
@@ -1,6 +1,8 @@
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main() {
     val playerName: String = "Estragon"
-    var experiencePoints: Int = 5
+    var experiencePoints = 5
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
